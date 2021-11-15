10be4c216475c25710e9727339b1b2fa93c73256
Naman <66850349+ExotizZn@users.noreply.github.com>
2021-11-15 19:41:58 +0100

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -37,7 +37,7 @@ module.exports = {
                 const embed = new MessageEmbed()
                     .setColor('GREEN')
                     .setTitle(':cd: Lecture en cours...')
-                    .setThumbnail(track.queue[0]['thumbnail']['url'])
+                    .setImage(track.queue[0]['thumbnail']['url'])
                     .addFields(
                         { name: ':notes: Titre', value: track.queue[0]['title'] },
                         { name: ':alarm_clock: Durée', value:  track.queue[0]['duration'] },
@@ -59,35 +59,24 @@ module.exports = {
                 highWaterMark: 1 << 25,
             })
 
-            if(data.queue.length == 1){
-                const resource = createAudioResource(source,{ inlineVolume:true })
-                resource.volume.setVolume(1)
-                const player = createAudioPlayer()
+            const resource = createAudioResource(source,{ inlineVolume:true })
+            resource.volume.setVolume(1)
 
-                const connection = joinVoiceChannel({
-                    channelId: channel.id,
-                    guildId: message.guild.id,
-                    adapterCreator: message.guild.voiceAdapterCreator,
-                });
+            queue.player.play(resource);
+            queue.player.on('idle',()=>{
+                queue.queue.shift()
+                const list = message.client.queue.get(message.guild.id);
 
-                data.player = player
-                data.connection = connection
-                connection.subscribe(player);
-                player.play(resource);
-                player.on('idle',()=>{
-                    data.queue.shift()
-                    const a = message.client.queue.get(message.guild.id);
-
-                    if (a.queue.length == 0){
-                        setTimeout(()=>{
-                            player.stop()
-                            connection.destroy()   
-                        },30000) 
-                    } else {
-                        play(a.queue[0])
-                    }
-                })
-            } 
+                if (list.queue.length == 0){
+                    setTimeout(()=>{
+                        queue.player.stop()
+                        queue.connection.destroy()   
+                    },30000) 
+                } else {
+                    play(list.queue[0])
+                }
+            })
+            
         } 
     }
 }
